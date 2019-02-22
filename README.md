97f71935c35fff5c66e24774e9c9c68849947339
Mike <mikhail.papkov@gmail.com>
2019-02-22 17:37:40 +0200

    Fix json decode bug for pythonanywhere

---
+++ README.md
@@ -0,0 +1,2 @@
+# Flask recaps collection
+
--- app/rating.py
+++ app/rating.py
@@ -8,19 +8,19 @@ api_root = 'http://rating.chgk.info/api'
 def get_team_info(idteam):
     app.logger.info('loading info about team ID {}'.format(idteam))
     url = '/'.join([api_root, 'teams', idteam])
-    response = json.loads(requests.get(url).text)
+    response = requests.get(url).json()
     return response[0]
 
 
 def get_base_recaps(idteam):
     app.logger.info('loading recaps team ID {}'.format(idteam))
     url = '/'.join([api_root, 'teams', idteam, 'recaps', 'last'])
-    response = json.loads(requests.get(url).text)
+    response = requests.get(url).json()
     return response
 
 
 def get_player_info(idplayer):
     app.logger.info('loading info for player ID {}'.format(idplayer))
     url = '/'.join([api_root, 'players', idplayer])
-    response = json.loads(requests.get(url).text)
+    response = requests.get(url).json()
     return response[0]
